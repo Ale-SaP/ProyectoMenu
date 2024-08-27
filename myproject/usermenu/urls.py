13bae0acc5e07e00c8e61ddc7490936d51bf1d45
--global <--global>
2024-08-27 17:41:26 -0300

    modal added

--- myproject/usermenu/urls.py
+++ myproject/usermenu/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('categories/', views.categories, name='categories'),
     path('content/<str:category>/', views.content, name='content'),
+    path('modal_content/<str:product_id>/', views.modal_content, name='modal_content'),
 ]
--- myproject/usermenu/views.py
+++ myproject/usermenu/views.py
@@ -1,6 +1,6 @@
 import os
 import json
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, Http404
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,3 +23,27 @@ def content(request, category):
         categories_data = json.load(file)
 
     return render(request, 'usermenu/content.html', {'rows': categories_data})
+
+def modal_content(request, product_id):
+    try:
+        category_id, prod_id = product_id.split('-')
+        loading_categories = os.path.join(BASE_DIR, 'mock-data', 'categories.json')
+        with open(loading_categories, 'r', encoding='utf-8') as file:
+            categories_data = json.load(file)
+
+        category_filter = next((item for item in categories_data if str(item['id']) == category_id), None)
+        
+        if not category_filter:
+            raise Http404("Category not found")
+        
+        category_content_path = os.path.join(BASE_DIR, 'mock-data/content', f'{category_filter["endpoint"]}.json')
+        with open(category_content_path, 'r', encoding='utf-8') as content_file:
+            category_content = json.load(content_file)
+        product_filter = next((item for item in category_content if str(item['id']) == prod_id), None)
+        if not product_filter:
+            raise Http404("Product not found")
+        return render(request, 'usermenu/modal_content.html', {'product': product_filter})
+
+    except (ValueError, FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error: {e}")
+        raise Http404("Invalid product or category")
